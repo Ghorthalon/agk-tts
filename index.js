af1e8c316a6759dfc5a7f2bef2e2b668f94490bb
The Geek on Skates <geek0nsk8s@gmail.com>
2018-09-18 22:03:37 -0400

    Set up the class to automatically create the ARIA component
    The class as it is requires the end-developer to create an
    element with id="speech" and the correct ARIA settings. But
    they might not know the right way to do that, and the whole
    point of this kind of abstraction is to keep things simple.
    So I figured the class should just do it for us. I've tested
    these changes on 62.0 (64-bit) and Chrome 68.0.3440.106
    (Official Build) (64-bit) using NVDA 2018.2 on Windows 10,
    and it worked great with both Web Speech and ARIA.

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 'use strict';
 
-/** Simple wrapper class for WebTTS, or additionally to update ARIA DOMNode to automatically read with screen reading technology.
-	* @param {boolean} webTTS - whether to use WebTTS or ARIA
-
-*/
-
+/** Text-to-speech using the Web Speech API or ARIA
+ * (ARIA-based speech will only be heard if using a
+ * screen reader, such as NVDA or VoiceOver)
+ */
 class TTS {
+	
+	/** Simple wrapper class for WebTTS, or additionally to update ARIA DOMNode to automatically read with screen reading technology.
+	 * @param {boolean} webTTS - whether to use WebTTS or ARIA
+	 */
 	constructor(webTTS = false) {
+		
+		// Set up Web Speech API stuff
 		this.synth = window.speechSynthesis;
 		this.webTTS = webTTS;
+
+		// Set up ARIA stuff
+		this.aria = document.createElement('div');
+		this.aria.id = "speech";
+		this.aria.style = "position: absolute; left: 0px; top: -400px";
+		this.aria.setAttribute("aria-live", "assertive");
+		document.body.appendChild(this.aria);
 	}
 
 	/** Speaks a string.
-	* @param {string} text - The text to be spoken.
-*/
+	 * @param {string} text - The text to be spoken.
+	 */
 	speak(text) {
 		if (this.webTTS) {
 			const utterThis = new SpeechSynthesisUtterance(text);
@@ -22,17 +34,17 @@ this.synth.stop();
 			}
 			this.synth.speak(utterThis);
 		} else {
-			document.getElementById('speech').innerHTML = '';
+			this.aria.innerHTML = '';
 			const para = document.createElement('p');
 			para.appendChild(document.createTextNode(text));
-			document.getElementById('speech').appendChild(para);
+			this.aria.appendChild(para);
 		}
 	}
-
 	// End speak()
+	
 	/** Sets webTTS to true or false.
-	* @param {boolean} tts - True to use WebTTS, false to use ARIA
-*/
+	 * @param {boolean} tts - True to use WebTTS, false to use ARIA
+	 */
 	setWebTTS(tts) {
 		this.webTTS = tts;
 	}
